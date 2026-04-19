d1654b3c45fee48b5c22cc9f8dba1b8167455d7c
AhmedSaber <ahmedomani2003@gmail.com>
2026-04-19 16:15:03 +0400

    ADD insert and select command

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "include/repl.h"
+#include "include/compiler.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -12,12 +13,28 @@ int main () {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer , ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command '%s'\n", input_buffer->buffer);
+        if (input_buffer->buffer[0] == '.') {
+            switch(do_meta_command(input_buffer)) {
+                case META_COMMAND_SUCCESS:
+                    continue;
+                case META_COMMAND_UNRECOGNIZED_COMMAND:
+                    printf("Unrecognized meta command '%s'\n", input_buffer->buffer);
+                    continue;
+            }
         }
+
+        Statement statement;
+
+        switch (prepare_statement(input_buffer, &statement)) {
+            case PREPARE_SUCCESS:
+                break;
+            case PREPARE_UNRECOGNIZED_STATEMENT:
+                printf("Unrecognized statement '%s'\n", input_buffer->buffer);
+                continue;
+        }
+
+        execute_statement(&statement);
+        printf("Command excuted successfully.\n");
     }
     return 0;
 }
---
+++ src/compiler.c
@@ -0,0 +1,34 @@
+#include "../include/compiler.h"
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+MetaCommandResult do_meta_command(InputBuffer *input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        close_input_buffer(input_buffer);
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement) {
+    if (strncmp(input_buffer->buffer , "insert" , 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer , "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+    if (statement->type == STATEMENT_INSERT) {
+        printf("This is an insert statement\n");
+    }
+    if (statement->type == STATEMENT_SELECT) {
+        printf("This is an select statement\n");
+    }
+}
