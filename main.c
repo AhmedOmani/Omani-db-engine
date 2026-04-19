2c3c1860346348fb568001642838cf3dc09c7e80
AhmedSaber <ahmedomani2003@gmail.com>
2026-04-19 15:17:00 +0400

    Implement REPL logic

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include "include/repl.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 int main () {
+    InputBuffer* input_buffer = new_input_buffer();
+
+    while (true) {
+        
+        print_prompt();
+        read_input(input_buffer);
+
+        if (strcmp(input_buffer->buffer , ".exit") == 0) {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        } else {
+            printf("Unrecognized command '%s'\n", input_buffer->buffer);
+        }
+    }
     return 0;
 }
